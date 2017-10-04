3b953cbe3d19a9c2b3d5e156dd0731e1e339d9f1
andro <androstoic@gmail.com>
2017-10-03 22:48:08 -0300

    update

--- functions/index.js
+++ functions/index.js
@@ -29,6 +29,7 @@ const nodemailer = require('nodemailer');
 const cors = require('cors')({origin: true});
 
 var qrProyect = require('qr-image');
+
 var promise = require('request-promise');
 var MP = require("mercadopago");
 
@@ -619,12 +620,234 @@ function sendWelcomeEmail(email, displayName) {
     const mailOptions = {
         from: `${APP_NAME} <noreply@firebase.com>`,
         to: email,
-        html: '<b>Hello world ?</b>'
+        html: `<!doctype html>
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
+
+<head>
+  <title></title>
+  <!--[if !mso]><!-- -->
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <!--<![endif]-->
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <style type="text/css">
+    #outlook a {
+      padding: 0;
+    }
+
+    .ReadMsgBody {
+      width: 100%;
+    }
+
+    .ExternalClass {
+      width: 100%;
+    }
+
+    .ExternalClass * {
+      line-height: 100%;
+    }
+
+    body {
+      margin: 0;
+      padding: 0;
+      -webkit-text-size-adjust: 100%;
+      -ms-text-size-adjust: 100%;
+    }
+
+    table,
+    td {
+      border-collapse: collapse;
+      mso-table-lspace: 0pt;
+      mso-table-rspace: 0pt;
+    }
+
+    img {
+      border: 0;
+      height: auto;
+      line-height: 100%;
+      outline: none;
+      text-decoration: none;
+      -ms-interpolation-mode: bicubic;
+    }
+
+    p {
+      display: block;
+      margin: 13px 0;
+    }
+  </style>
+  <!--[if !mso]><!-->
+  <style type="text/css">
+    @media only screen and (max-width:480px) {
+      @-ms-viewport {
+        width: 320px;
+      }
+      @viewport {
+        width: 320px;
+      }
+    }
+  </style>
+  <!--<![endif]-->
+  <!--[if mso]>
+<xml>
+  <o:OfficeDocumentSettings>
+    <o:AllowPNG/>
+    <o:PixelsPerInch>96</o:PixelsPerInch>
+  </o:OfficeDocumentSettings>
+</xml>
+<![endif]-->
+  <!--[if lte mso 11]>
+<style type="text/css">
+  .outlook-group-fix {
+    width:100% !important;
+  }
+</style>
+<![endif]-->
+
+  <!--[if !mso]><!-->
+  <link href="https://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700" rel="stylesheet" type="text/css">
+  <style type="text/css">
+    @import url(https://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700);
+  </style>
+  <!--<![endif]-->
+  <style type="text/css">
+    @media only screen and (min-width:480px) {
+      .mj-column-per-100 {
+        width: 100%!important;
+      }
+      .mj-column-px-300 {
+        width: 300px!important;
+      }
+    }
+  </style>
+</head>
+
+<body>
+
+  <div class="mj-container">
+    <!--[if mso | IE]>
+      <table role="presentation" border="0" cellpadding="0" cellspacing="0" width="600" align="center" style="width:600px;">
+        <tr>
+          <td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;">
+      <![endif]-->
+    <div style="margin:0px auto;max-width:600px;">
+      <table role="presentation" cellpadding="0" cellspacing="0" style="font-size:0px;width:100%;" align="center" border="0">
+        <tbody>
+          <tr>
+            <td style="text-align:center;vertical-align:top;direction:ltr;font-size:0px;padding:20px 0px;">
+              <!--[if mso | IE]>
+      <table role="presentation" border="0" cellpadding="0" cellspacing="0">
+        <tr>
+          <td style="vertical-align:top;width:600px;">
+      <![endif]-->
+              <div class="mj-column-per-100 outlook-group-fix" style="vertical-align:top;display:inline-block;direction:ltr;font-size:13px;text-align:left;width:100%;">
+                <table role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
+                  <tbody>
+                    <tr>
+                      <td style="word-wrap:break-word;font-size:0px;padding:20px 0px;">
+                        <div style="margin:0px auto;max-width:600px;background:#1b001b;" data-class="">
+                          <table role="presentation" cellpadding="0" cellspacing="0" style="font-size:0px;width:100%;background:#1b001b;" align="center" border="0">
+                            <tbody>
+                              <tr>
+                                <td style="text-align:center;vertical-align:top;direction:ltr;font-size:0px;padding:20px 0px;">
+                                  <!--[if mso | IE]>
+      <table role="presentation" border="0" cellpadding="0" cellspacing="0">
+        <tr>
+          <td style="vertical-align:top;width:300px;">
+      <![endif]-->
+                                  <div class="mj-column-px-300 outlook-group-fix" style="vertical-align:top;display:inline-block;direction:ltr;font-size:13px;text-align:left;width:100%;">
+                                    <table role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
+                                      <tbody>
+                                        <tr>
+                                          <td style="word-wrap:break-word;font-size:0px;padding:10px 25px;" align="center">
+                                            <table role="presentation" cellpadding="0" cellspacing="0" style="border-collapse:collapse;border-spacing:0px;" align="center" border="0">
+                                              <tbody>
+                                                <tr>
+                                                  <td style="width:50px;"><img alt="" title="" height="auto" src="https://izinait.com/app/logo.png" style="border:none;border-radius:0px;display:block;font-size:13px;outline:none;text-decoration:none;width:100%;height:auto;" width="50"></td>
+                                                </tr>
+                                              </tbody>
+                                            </table>
+                                          </td>
+                                        </tr>
+                                        <tr>
+                                          <td style="word-wrap:break-word;font-size:0px;padding:10px 25px;" align="center">
+                                            <div style="cursor:auto;color:#fff;font-family:Helvetica Neue;font-size:30px;line-height:22px;text-align:center;">izinait</div>
+                                          </td>
+                                        </tr>
+                                        <tr>
+                                          <td style="word-wrap:break-word;font-size:0px;padding:10px 25px;" align="center">
+                                            <table role="presentation" cellpadding="0" cellspacing="0" style="border-collapse:separate;" align="center" border="0">
+                                              <tbody>
+                                                <tr>
+                                                  <td style="border:none;border-radius:3px;color:#ffffff;cursor:auto;padding:10px 25px;" align="center" valign="middle" bgcolor="#F63A4D"><a href="https://izinait.com/app/#!/codigo" style="text-decoration:none;background:#F63A4D;color:#ffffff;font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:13px;font-weight:normal;line-height:120%;text-transform:none;margin:0px;"
+                                                      target="_blank">Ver tus tickets</a></td>
+                                                </tr>
+                                              </tbody>
+                                            </table>
+                                          </td>
+                                        </tr>
+                                        <tr>
+                                          <td style="word-wrap:break-word;font-size:0px;padding:10px 25px;" align="left">
+                                            <div style="cursor:auto;color:white;font-family:helvetica;font-size:13px;line-height:22px;text-align:left;">Gracias Andro Ostoic por comprar en izinait</div>
+                                          </td>
+                                        </tr>
+                                        <tr>
+                                          <td style="word-wrap:break-word;font-size:0px;padding:10px 25px;" align="left">
+                                            <div style="cursor:auto;color:white;font-family:helvetica;font-size:11px;line-height:22px;text-align:left;">Acabas de realizar una compra para el evento "NOMBRE EVENTO", con un valor de "VALOR TICKETS" , recuerda que con tu codigo QR podras acceder de manera rapida y sencilla.</div>
+                                          </td>
+                                        </tr>
+                                        <tr>
+                                          <td style="word-wrap:break-word;font-size:0px;padding:10px 25px;" align="center">
+                                            <table role="presentation" cellpadding="0" cellspacing="0" style="border-collapse:collapse;border-spacing:0px;" align="center" border="0">
+                                              <tbody>
+                                                <tr>
+                                                  <td style="width:250px;"><img alt="" title="" height="auto" src="https://firebasestorage.googleapis.com/v0/b/project-8746388695669481444.appspot.com/o/eventImages%2F-KuutzdO0BnoF8J_HqPV?alt=media&token=d1fc104f-458f-4be0-9f8d-5cbd7095c3c3"
+                                                      style="border:none;border-radius:0px;display:block;font-size:13px;outline:none;text-decoration:none;width:100%;height:auto;" width="250"></td>
+                                                </tr>
+                                              </tbody>
+                                            </table>
+                                          </td>
+                                        </tr>
+                                        <tr>
+                                          <td style="word-wrap:break-word;font-size:0px;padding:10px 25px;" align="left">
+                                            <div style="cursor:auto;color:white;font-family:helvetica;font-size:9px;line-height:22px;text-align:left;">Nombre Evento : The Nuñork Times <br> Club Evento : Caleido Club<br> Comprado el: 25 sept. 2017 / 07:13<br> Código Transacción: trx_jw1a-HdW_9DdCL8eW5M0GA <br> Correo del comprador : andro.ostoic@gmail.com <br>                                              Metodo de pago : OneClick</div>
+                                          </td>
+                                        </tr>
+                                      </tbody>
+                                    </table>
+                                  </div>
+                                  <!--[if mso | IE]>
+      </td></tr></table>
+      <![endif]-->
+                                </td>
+                              </tr>
+                            </tbody>
+                          </table>
+                        </div>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <!--[if mso | IE]>
+      </td></tr></table>
+      <![endif]-->
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <!--[if mso | IE]>
+      </td></tr></table>
+      <![endif]-->
+  </div>
+</body>
+
+</html>`
     };
 
     // The user subscribed to the newsletter.
     mailOptions.subject = `Welcome to ${APP_NAME}!`;
-    mailOptions.text = `Hey ${displayName || ''}! Welcome to ${APP_NAME}. I hope you will enjoy our service.`
+    mailOptions.text = ``
     ;
     return mailTransport.sendMail(mailOptions).then(() => {
         console.log('New welcome email sent t1111111o:', email);
